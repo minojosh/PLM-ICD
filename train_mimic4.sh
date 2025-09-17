8016c4f03b180542aec31a5490801a31e5c01920
minojosh <joshua@minohealth.org>
2025-09-17 11:06:49 +0000

    chore(train): update training script to use accelerate with CUDA config
    - Comment out direct python run command for training
    - Add PYTORCH_CUDA_ALLOC_CONF environment variable with expandable_segments enabled
    - Use accelerate launch to run the training script for improved resource management

--- train_mimic4.sh
+++ train_mimic4.sh
@@ -19,7 +19,8 @@ else
     exit 1
 fi
 
-python3 run_icd.py \
+# python3 run_icd.py \
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True accelerate launch run_icd.py 
     --train_file ../data/mimic4/mimic4_icd10/train_full.csv \
     --validation_file ../data/mimic4/mimic4_icd10/dev_full.csv \
     --max_length 512 \
